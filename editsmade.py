1eaf893215031dfc967521d00edac7e333e31ff0
James Harper <james.harper.us@gmail.com>
2015-06-22 21:20:00 -0700

    Incremental update

--- editsmade.py
+++ editsmade.py
@@ -1,17 +1,26 @@
-string1 = "Hello. My name is James Harper. This is my text parsing and comparison program, editsmade."
+string1 = "Hello. Me name is James Harper. This is my text parsing and comparison program, editsmade."
 string2 = "Hello. My name is James Harper. This is my text parsing and comparison program, editsmade."
 
-words = []
-start = 0
+words1 = string1.split()
+words2 = string2.split()
 
-for i in range(len(string1)):
-    if string1[i] == " ":
-        if start == 0:
-            start = i
-        elif start != 0:
-            print i
-            print string1[start:i + 1]
-            words.append(string1[start:(i + 1)])
-            start = 0
-        else:
-            print "ERROR"
+diff1 = []
+diff2 = []
+
+loc = []
+
+for i in range(len(words1)):
+    if words1[i] != words2[i]:
+        diff1.append(words1[i])
+        diff2.append(words2[i])
+        loc.append(i)
+        print words1[i], "--->", words2[i]
+
+for i in range(len(loc)-1):
+    if loc[i] == loc[i + 1] - 1:
+        diff1[i] += " " + diff1[i + 1]
+        diff2[i] += " " + diff2[i + 1]
+        del diff1[i + 1]
+        del diff2[i + 1]
+
+print diff1, diff2
